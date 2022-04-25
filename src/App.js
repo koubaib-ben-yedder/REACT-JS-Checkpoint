8efdb29d2f77a9fa8261edd137bc7586888bb8f2
ben yedder <benyedderkoubaib@gmail.com>
2022-04-25 13:46:57 +0100

    add navbar and card and table compoment to main compoment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 
+import CustomCard from "./compoment/ui/card/card";
+import CustomNavbar from "./compoment/navbar";
+import CustomTable from "./compoment/ui/table/table";
+
 
 function App() {
   return (
     <div className="App">
+
+      <CustomNavbar />
+
+      <CustomCard />
+
+      <CustomTable />
       
     </div>
   );
